5e465f6666f23d369b4df4bad8e792ff65b210d6
Aizaz Hassan <aizazhassan1999@gmail.com>
2022-05-09 19:48:30 +0300

    changed backend base url to heroku

--- src/axios.jsx
+++ src/axios.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { BACKEND_BASE_URL } from "./constants/others";
 
+axios.defaults.withCredentials = true;
+
 const instance = axios.create({
   baseURL: BACKEND_BASE_URL,
   withCredentials: true,
